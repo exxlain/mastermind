d59e01db19b2e8687df2c65c67f06aaf01dfcade
Svetlana Kostina <kostina@sibext.com>
2022-02-23 19:24:03 +0700

    [added] redux-persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,45 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action, combineReducers } from '@reduxjs/toolkit';
 import startPageReducer from '../features/StartPage/startPageSlice';
 import gamePageReducer from '../features/GamePage/gamePageSlice';
 import counterReducer from '../features/counter/counterSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  whitelist: ['gamePage', 'startPage'],
+  storage,
+};
+
+const rootReducer =  combineReducers({
+  startPage: startPageReducer,
+  gamePage: gamePageReducer,
+  counter: counterReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    startPage: startPageReducer,
-    gamePage: gamePageReducer,
-    counter: counterReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
+
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
